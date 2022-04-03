10f176d01b9548560730afbdf527b11d272a6181
Miqueas Martinez <miqueas2020@yahoo.com>
2022-04-03 16:17:35 -0500

    Minor changes in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+# Removes the "welcome" thing
+set fish_greeting
+
 set -gx NIMPATH "$HOME/.nimble/bin"
+set -gx SWIFTPATH "$HOME/.swift/usr/bin"
 set -gx DISTROBOXPATH "$HOME/.distrobox/bin"
-set -a PATH $NIMPATH $DISTROBOXPATH
+set -a PATH $NIMPATH $SWIFTPATH $DISTROBOXPATH
