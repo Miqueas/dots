bb4e010058ac3396fcc4a8a1b7d0568a7d9d0859
Miqueas Martinez <miqueas2020@yahoo.com>
2022-04-12 20:55:49 -0500

    Added 'ls' alias to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ set -gx NIMPATH "$HOME/.nimble/bin"
 set -gx SWIFTPATH "$HOME/.swift/usr/bin"
 set -gx DISTROBOXPATH "$HOME/.distrobox/bin"
 set -a PATH $NIMPATH $SWIFTPATH $DISTROBOXPATH
+
+alias ls "ls -lhAF --color"
