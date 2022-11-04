b4fb002beeb1c7b42a6c1c9fc8614c36de6bd30e
Miqueas <miqueas2020@yahoo.com>
2022-11-03 20:35:27 -0500

    E

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,9 @@ set fish_greeting
 
 set -l LocalBins "$HOME/.local/bin"
 set -l NIMPATH "$HOME/.nimble/bin"
-set -gx DENO_INSTALL_ROOT "$HOME/.deno/bin"
+#set -gx DENO_INSTALL_ROOT "$HOME/.deno/bin"
 
-for p in $LocalBins $NIMPATH $DENO_INSTALL_ROOT
+for p in $LocalBins $NIMPATH
   if not contains $p $PATH
     set -a PATH $p
   end
@@ -34,13 +34,13 @@ end
 # I have a problem with flatpak related to $XDG_DATA_DIRS, afaik, this
 # is because I'm using fish as my shell instead of bash or something
 # POSIX-compatible
-set -l --path DataDirs /usr/share/kde-settings/kde-profile/default/share /usr/local/share /usr/share
-set -gx --path XDG_DATA_DIRS $DataDirs "/var/lib/flatpak/exports/share" "$DataHome/flatpak/exports/share"
+#set -l --path DataDirs /usr/share/kde-settings/kde-profile/default/share /usr/local/share /usr/share
+#set -gx --path XDG_DATA_DIRS $DataDirs "/var/lib/flatpak/exports/share" "$DataHome/flatpak/exports/share"
 
-set -l FlatpakBins "/var/lib/flatpak/exports/bin" "$DataHome/flatpak/exports/bin"
+#set -l FlatpakBins "/var/lib/flatpak/exports/bin" "$DataHome/flatpak/exports/bin"
 
-for FDir in $FlatpakBins
-  if not contains $FDir $PATH
-    set -a PATH $FDir
-  end
-end
+#for FDir in $FlatpakBins
+#  if not contains $FDir $PATH
+#    set -a PATH $FDir
+#  end
+#end
