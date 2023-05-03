7c8dab0b88903405b048760f23f589c196b4ca82
Miqueas <miqueas2020@yahoo.com>
2023-05-03 09:53:13 -0500

    Updated Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,12 +16,17 @@ alias cat bat
 
 set -l localBins "$HOME/.local/bin"
 set -l nimbleBins "$HOME/.nimble/bin"
+set -l flutterBins "$HOME/.local/flutter/bin"
 # set -x BUN_INSTALL "$HOME/.bun"
 
-for p in $localBins "$localBins/gcm" $nimbleBins
+for p in $localBins "$localBins/gcm" $nimbleBins $flutterBins
   if not contains $p $PATH
     set -a PATH $p
   end
+end
+
+if not set -q CHROME_EXECUTABLE
+  set -x CHROME_EXECUTABLE google-chrome-stable
 end
 
 #set -x PATH $PATH $LocalBins $NimbleBins $BUN_INSTALL/bin
