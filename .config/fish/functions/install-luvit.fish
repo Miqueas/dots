6e1cd85353a464bfd6ec9f04852065515fdca93b
Miqueas <miqueas2020@yahoo.com>
2023-10-01 15:55:10 -0500

    Updated install-luvit function

--- .config/fish/functions/install-luvit.fish
+++ .config/fish/functions/install-luvit.fish
@@ -3,28 +3,46 @@ function install-luvit
   set -l luviVersion  "2.14.0"
   set -l luvitVersion "2.18.1"
 
+  echo "Installing Luvit $luvitVersion with Lit $litVersion and Luvi $luviVersion"
+
   set -l litUrl   https://lit.luvit.io/packages/luvit/lit/v$litVersion.zip
   set -l luviUrl  https://github.com/luvit/luvi/releases/download/v$luviVersion/luvi-regular-Linux_x86_64
   set -l luvitUrl https://lit.luvit.io/packages/luvit/luvit/v$luvitVersion.zip
 
+  echo "Temporary changing to $HOME"
   cd $HOME
 
+  echo "Downloading Lit and Luvit"
   wget -qO lit.zip   $litUrl
   wget -qO luvit.zip $luvitUrl
+  
+  __downloadLuvi $luviUrl
 
-  wget -qO luvi $luviUrl
-  chmod +x luvi
-
+  echo "Building Lit with Luvi"
   ./luvi lit.zip -- make lit.zip ./lit ./luvi
 
-  wget -qO luvi $luviUrl
-  chmod +x luvi
+  __downloadLuvi $luviUrl
 
+  echo "Building Luvit with Lit"
   ./lit make luvit.zip ./luvit ./luvi
 
-  wget -qO luvi $luviUrl
-  chmod +x luvi
+  __downloadLuvi $luviUrl
 
+  if not [ -d $HOME/.local/bin ]
+    mkdir -p $HOME/.local/bin
+  end
+
+  echo "Moving Lit, Luvi and Luvit to $HOME/.local/bin"
   mv lit luvi luvit $HOME/.local/bin
+  echo "Cleaning up"
   rm lit.zip luvit.zip
+
+  echo "Done!"
+end
+
+function __downloadLuvi
+  echo "Downloading Luvi"
+  wget -qO luvi $argv[1]
+  echo "Changing permissions to Luvi"
+  chmod +x luvi
 end
